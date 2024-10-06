80c2750d96fbb2d04c35cb87e28bd697e0054532
unknown <dimiendrixmmiranda@gmail.com>
2024-10-06 20:49:32 -0300

    update

--- src/app/menus/skins/page.tsx
+++ src/app/menus/skins/page.tsx
@@ -1,5 +1,5 @@
-import SkinsCS2 from "@/components/skins/Skins";
-
+'use client'
+import SkinsCS2 from "@/components/skins/SkinsCS2";
 export default function Page(){
     return (
         <SkinsCS2></SkinsCS2>
